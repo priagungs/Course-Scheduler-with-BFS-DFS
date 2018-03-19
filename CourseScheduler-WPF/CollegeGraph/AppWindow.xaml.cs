5e1145d6fc9ee0d861b0d0fd6f7276258c01c907
priagungs <32959030+priagungs@users.noreply.github.com>
2018-03-19 18:51:44 +0700

    added graphsharp

---
+++ CourseScheduler-WPF/CollegeGraph/AppWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Controls;
+
+namespace CollegeGraph
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private String fileName;
+        private Graph scheduleGraph;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool hasBeenClicked = false;
+
+        private void TextBox_Focus(object sender, RoutedEventArgs e)
+        {
+            if (!hasBeenClicked)
+            {
+                TextBox box = sender as TextBox;
+                box.Text = String.Empty;
+                hasBeenClicked = true;
+            }
+        }
+
+        private void Print_Solution()
+        {
+            String s = "";
+            int smt = 1;
+            s += "Semester " + smt + ": ";
+            int[] smtCourses = new int[scheduleGraph.solution.Length];
+            for (int i = 0; i < smtCourses.Length; i++)
+            {
+                smtCourses[i] = -1;
+            }
+            int idSmtCourses = 0;
+            smtCourses[idSmtCourses] = scheduleGraph.solution[0];
+            for (int i = 1; i < Graph.graphEl.Length; i++)
+            {
+                bool proper = true;
+                foreach (int element in smtCourses)
+                {
+                    if (element != -1 && Graph.graph[element, scheduleGraph.solution[i]])
+                    {
+                        proper = false;
+                    }
+                }
+                if (proper)
+                {
+                    idSmtCourses++;
+                    smtCourses[idSmtCourses] = scheduleGraph.solution[i];
+                }
+                else
+                {
+                    foreach (int element in smtCourses)
+                    {
+                        if (element != -1)
+                        {
+                            s += Graph.graphEl[element] + " ";
+                        }
+                    }
+                    idSmtCourses = 0;
+                    for (int j = 0; j < smtCourses.Length; j++)
+                    {
+                        smtCourses[j] = -1;
+                    }
+                    smtCourses[idSmtCourses] = scheduleGraph.solution[i];
+                    smt++;
+                    s += "\nSemester " + smt + ": ";
+                }
+            }
+            foreach (int element in smtCourses)
+            {
+                if (element != -1)
+                {
+                    s += Graph.graphEl[element] + " ";
+                }
+            }
+            textBoxSol.Text = s;
+        }
+
+        private void DFS_Button_Click(object sender, RoutedEventArgs e)
+        {
+            scheduleGraph = new Graph(fileName);
+            scheduleGraph.DFSSolution();
+            Print_Solution();
+            GView graphView = new GView();
+            graphView.Show();
+            graphView.Animate();
+
+
+        }
+
+        private void BFS_Button_Click(object sender, RoutedEventArgs e)
+        {
+            scheduleGraph = new Graph(fileName);
+            scheduleGraph.BFSSolution();
+            Print_Solution();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                fileName = System.IO.Path.GetFullPath(openFileDialog.FileName);
+                directoryName.Text = fileName;
+
+            }
+        }
+    }
+}
---
+++ CourseScheduler-WPF/CollegeGraph/GraphView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using QuickGraph;
+using GraphSharp;
+using System;
+
+namespace CollegeGraph
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class GView : Window
+    {
+        public BidirectionalGraph<object, IEdge<object>> graph;
+        private IBidirectionalGraph<object, IEdge<object>> _g2Visual;
+
+        public IBidirectionalGraph<object, IEdge<object>> g2Visual {
+            get { return _g2Visual; }
+        }
+
+        public GView()
+        {
+            CreateGraph();
+            InitializeComponent();
+            Animate();
+        }
+
+        private void CreateGraph() {
+            graph = new BidirectionalGraph<object, IEdge<object>>();
+            _g2Visual = graph;  
+            
+        }
+        public void Animate() {
+            string[] vertices = Graph.graphEl;
+            bool[,] adjmx = Graph.graph;
+            foreach (int element in Graph.visitedNode)
+            {
+                Console.WriteLine(element);
+                graph.AddVertex(vertices[element]);
+            }
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                for (int j = 0; j < vertices.Length; j++)
+                {
+                    if (adjmx[i, j])
+                    {
+                        graph.AddEdge(new Edge<object>(vertices[i], vertices[j]));
+                    }
+                }
+            }
+            _g2Visual = graph;
+
+
+        }
+    }
+}
